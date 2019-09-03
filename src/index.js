c7779ab25706c72630421abdb21984463e9be736
Adm1ralDen <games.zadrotina@gmail.com>
2019-09-03 23:15:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,109 +1,139 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { render } from 'react-dom';
-// import Portal from './Components/Portal';
-import { Stage, Layer, Rect, Group, Arrow } from 'react-konva';
+import Portal from './Components/Portal';
+import { Stage, Layer, Rect, Group, Arrow, Tag, Text, Label } from 'react-konva';
 
 
 function ColoredRect(props) {
-    const [color] = useState('green');
     let position = props.position;
-
+    let index = props.index;
     let handleDrag = (propsValue) => {
-        props.setPosition([propsValue.currentTarget.attrs.y, propsValue.currentTarget.attrs.x]);
+        props.setPosition(
+            position.map((item, Zindex) => {
+                if (index === Zindex) return [propsValue.currentTarget.attrs.y, propsValue.currentTarget.attrs.x];
+                return item;
+            })
+        );
     }
 
-    return (<Rect
-        // name="rectange-name"
-        x={position[1]}
-        y={position[0]}
-        width={100}
-        height={100}
-        fill={color}
-        shadowBlur={5}
-        draggable
-        onDragMove={handleDrag}
-        name='fillShape'
-    />
+    return (
+        <Group>
+            <Rect
+                // name="rectange-name"
+                x={position[index][1]}
+                y={position[index][0]}
+                width={100}
+                height={100}
+                fill='green'
+                shadowBlur={5}
+                draggable
+                onDragMove={handleDrag}
+                name='fillShape'
+            />
+            {/* <Label
+            x={position[index][1]}
+            y={position[index][0]}
+            opacity={0.75}
+        >
+            <Tag
+                fill='black'
+                pointerDirection='down'
+                pointerWidth={10}
+                pointerHeight={10}
+                lineJoin='round'
+                shadowColor='black'
+                shadowBlur={10}
+                shadowOffset={10}
+                shadowOpacity={0.5}
+            >
+                <Text
+                    text={'x = ' + position[index][1] + ', y = ' + position[index][0]}
+                    fontFamily='Calibri'
+                    fontSize={18}
+                    padding={5}
+                    fill='white'
+                />
+            </Tag>
         // <Group>
 
-        //     {/* інпут прибра для кращої візуалізації */}
-        //     {/* <Portal>
-        //         <div>
-        //             <input style={{
-        //                 position: 'absolute',
-        //                 top: position[0] + 50,
-        //                 left: position[1] + 6,
-        //                 width: '100px'
-        //             }}
-        //                 type='text' placeholder='some text'></input>
-        //         </div>
-        //     </Portal> */}
-        // </Group>
+            {/* інпут прибра для кращої візуалізації */}
+            <Portal>
+                <div>
+                    <input style={{
+                        position: 'absolute',
+                        top: position[index][0] + 50,
+                        left: position[index][1] + 6,
+                        width: '100px'
+                    }}
+                        type='text' placeholder={'x = ' + position[index][1] + ', y = ' + position[index][0]}></input>
+                </div>
+            </Portal>
+        </Group>
     );
 
 }
 function ArrowComponent(props) {
-    let positionStart = props.positionStart, positionEnd = props.positionEnd;
-    let handleMove = (e) => {
-
-    }
-
+    let [y1, x1] = props.positionStart, [y2, x2] = props.positionEnd;
+    // let handleMove = (e) => {
+    let widthAndHeight = props.widthAndHeight;
+    // }
     return (
         <Arrow
-            points={[positionStart[1] + 50, positionStart[0] + 50, positionEnd[1] + 50, positionEnd[0] + 50]}
+            points={[x1 + 50, y1 + 120, x2 - 20, y2 + 50]}
             pointerLength={10}
             pointerWidth={10}
             fill='black'
             stroke='black'
             strokeWidth={4}
-            onDragMove={handleMove}
+        // onDragMove={handleMove}
         />
     )
 }
 function Shape(props) {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
     const groupEl = useRef(null);
     const layerEl = useRef(null);
-    const [positionRect1, setPositionRect1] = useState([100, 100]);
-    const [positionRect2, setPositionRect2] = useState([300, 300]);
-    const [positionRect3, setPositionRect3] = useState([100, 500]);
+    const arrowGroupEl = useRef(null);
+    const stageEl = useRef(null);
+    const [positionShape, setPositionShape] = useState([[100, 100], [300, 300], [100, 500]]);
     let handDragMoveLayer = (e) => {
         let target = e.target;
-        let targetRect = e.target.getClientRect();
+        let targetRect = target.getClientRect();
+        let pos = stageEl.current.getPointerPosition();
+        let shape = layerEl.current.getIntersection(pos);
+       if( shape !== null) console.log(targetRect.x, targetRect.y);
+        // let arrow = arrowGroupEl.current.children[0];
         groupEl.current.children.forEach(function (group) {
-            if (group === target) {
-                return;
-            }
-            console.log(group);
-            if (haveIntersection(group.getClientRect(), targetRect)) {
-                group.fill('red');
+            if (haveIntersection(group.children[0].getClientRect()) /*targetRect)*/) {
+                group.children[0].fill('red');
             } else {
-                group.fill('green');
+                group.children[0].fill('green');
             }
         });
     }
-    let haveIntersection = (r1, r2) => {
-        return !(
-            r2.x > r1.x + r1.width ||
-            r2.x + r2.width < r1.x ||
-            r2.y > r1.y + r1.height ||
-            r2.y + r2.height < r1.y
-        );
+    let haveIntersection = (r1) => {
+        let shape = layerEl.current.getIntersection({x: r1.x, y: r1.y});
+        if(shape !== null) console.log(shape);
     }
+
     return (
-        <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Stage 
+        width={window.innerWidth} 
+        height={window.innerHeight} 
+        ref={stageEl}
+        >
             <Layer
                 ref={layerEl}
                 onDragMove={handDragMoveLayer}
             >
                 <Group ref={groupEl} >
-                    <ColoredRect position={positionRect1} setPosition={setPositionRect1} />
-                    <ColoredRect position={positionRect2} setPosition={setPositionRect2} />
-                    <ColoredRect position={positionRect3} setPosition={setPositionRect3} />
+                    {positionShape.map((item, index) => {
+                        return <ColoredRect key={index} position={positionShape} index={index} setPosition={setPositionShape} />
+                    })}
                 </Group>
-                <ArrowComponent positionStart={positionRect1} positionEnd={positionRect2} />
-                <ArrowComponent positionStart={positionRect2} positionEnd={positionRect3} />
+                <Group ref={arrowGroupEl}>
+                    <ArrowComponent positionStart={positionShape[0]} positionEnd={positionShape[1]} />
+                    {/* <ArrowComponent positionStart={positionRect2} positionEnd={positionRect3} /> */}
+                </Group>
             </Layer>
         </Stage>
     );
